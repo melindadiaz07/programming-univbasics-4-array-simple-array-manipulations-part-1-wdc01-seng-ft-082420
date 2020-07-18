223804bf05f0732736e7d2c5571b82d286ce6ee4
Melinda Diaz <melindadiaz@me.com>
2020-07-18 19:05:33 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,7 @@
+def using_push(colors_in_the_rainbow, next_color)
+  updated_array = colors_in_the_rainbow.push(next_color)
+end
+
+def using_unshift(boroughs_in_nyc, @new_neighborhood)
+  updated
+end
