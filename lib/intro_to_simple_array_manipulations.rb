872bcecf4e215db9e5894808c4188cfa2dd83a41
Melinda Diaz <melindadiaz@me.com>
2020-07-18 19:19:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -18,4 +18,9 @@ def using_shift(my_favorite_cities)
   im_so_over_this_city = my_favorite_cities.shift()
 end
 
-def shift_with_args()
+def shift_with_args(ice_cream_brands)
+  ice_cream_brands.shift(2)
+end
+  
+  
+  
