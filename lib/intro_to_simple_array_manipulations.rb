107b757c2da238016985a24a20c1c77a9e08ce08
Melinda Diaz <melindadiaz@me.com>
2020-07-18 19:20:03 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,26 @@
+def using_push(colors_in_the_rainbow, next_color)
+  colors_in_the_rainbow.push(next_color)
+end
+
+def using_unshift(boroughs_in_nyc, new_neighborhood)
+  boroughs_in_nyc.unshift(new_neighborhood)
+end
+
+def using_pop(continents)
+  continents.pop()
+end
+
+def pop_with_args(dog_breeds)
+  dog_breeds.pop(2)
+end
+
+def using_shift(my_favorite_cities)
+  im_so_over_this_city = my_favorite_cities.shift()
+end
+
+def shift_with_args(ice_cream_brands)
+  ice_cream_brands.shift(2)
+end
+  
+  
+  
